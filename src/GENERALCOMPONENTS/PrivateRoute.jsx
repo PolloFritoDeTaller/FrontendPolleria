1b3cd9df4e7ef48ff60fe0f6c75ac9608498ac15
Valeria Rivero <valeria.rivero@ucb.edu.bo>
2024-12-19 14:56:38 -0400

    pruebas de authcontext

--- src/GENERALCOMPONENTS/PrivateRoute.jsx
+++ src/GENERALCOMPONENTS/PrivateRoute.jsx
@@ -2,13 +2,14 @@ import { Navigate } from "react-router-dom";
 import { useAuth } from "./AuthContext";
 
 const PrivateRoute = ({ children, allowedRoles }) => {
-  const { isAuthenticated, isLoading, user } = useAuth(); // Accede a `user` del contexto de autenticación
+  const { isAuthenticated, isLoading, user } = useAuth(); // Accede al contexto
 
   if (isLoading) return <div>Cargando...</div>;
 
   // Verifica si el usuario está autenticado y si su rol está dentro de los permitidos
-  if (!isAuthenticated || !allowedRoles.includes(user?.role)) {
-    return <Navigate to="/login" />; // O redirigir a otra página si no tiene acceso
+  if (!isAuthenticated || (allowedRoles && !allowedRoles.includes(user?.role))) {
+    // Si no está autenticado o no tiene el rol adecuado, no redirige, solo retorna null
+    return null; // Deberías manejar la lógica de mostrar algo alternativo (p. ej., un mensaje)
   }
 
   return children;
