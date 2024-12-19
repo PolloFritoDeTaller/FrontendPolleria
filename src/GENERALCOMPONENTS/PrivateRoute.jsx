3115594992d6409a8d2ec9a9194e4d1e4ca1eb6c
Valeria Rivero <valeria.rivero@ucb.edu.bo>
2024-12-19 15:30:33 -0400

    pruebas de authcontext

--- src/GENERALCOMPONENTS/PrivateRoute.jsx
+++ src/GENERALCOMPONENTS/PrivateRoute.jsx
@@ -1,16 +1,16 @@
-import { Navigate, Outlet } from "react-router-dom";
-import { useAuth } from "../GENERALCOMPONENTS/AuthContext";
-import { Children } from "react";
+import { Navigate } from "react-router-dom";
+import { useAuth } from "./AuthContext";
 
-const PrivateRoute = ({ allowedRoles }) => {
-  const { isAuthenticated, isLoading, user } = useAuth();
 
-  if (isLoading) return <div>Cargando...</div>;
-
+const PrivateRoute = ({ children, allowedRoles }) => {
+    const { isAuthenticated, isLoading, user } = useAuth(); // Accede a `user` del contexto de autenticación
   
-
-  // Si el usuario está autenticado y tiene permiso, renderiza las rutas hijas
-  return Children;
-};
+    if (isLoading) return <div>Cargando...</div>;
+  
+    // Verifica si el usuario está autenticado y si su rol está dentro de los permitidos
+    return allowedRoles.includes(user?.role)
+      ? children
+      : <Navigate to="/index" />;
+  };
 
 export default PrivateRoute;
