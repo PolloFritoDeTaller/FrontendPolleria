699962be98448a5054a2f14d0a8f8b207ba7a4cb
Valeria Rivero <valeria.rivero@ucb.edu.bo>
2024-12-19 02:45:33 -0400

    Prueba responsividad

--- src/GENERALCOMPONENTS/Header.jsx
+++ src/GENERALCOMPONENTS/Header.jsx
@@ -82,11 +82,8 @@ const Header = () => {
       {(userRole === 'admin' || userRole === 'client' || userRole === 'worker') && (
         <header className="fixed top-0 left-0 right-0 flex flex-col md:flex-row items-center justify-between p-4 bg-red-600 text-white shadow-lg space-y-2 md:space-y-0 z-50">
           {/* Logo y Menú de navegación */}
-          <div className="flex items-center justify-between w-full md:w-auto relative z-60">
-            <button
-              onClick={toggleNavBar}
-              className="text-3xl hover:text-yellow-300 transition-colors z-60"
-            >
+          <div className="flex items-center justify-between w-full md:w-auto">
+            <button onClick={toggleNavBar} className="text-3xl hover:text-yellow-300 transition-colors">
               <FaBars />
             </button>
             <Link
@@ -98,15 +95,15 @@ const Header = () => {
           </div>
 
           {/* Menú de perfil, sucursales y carrito */}
-          <div className={`flex flex-col md:flex-row items-center space-x-0 md:space-x-4 w-full md:w-auto mt-2 md:mt-0`}>
+          <div className="flex items-center space-x-4">
             {/* Selección de sucursales */}
-            <div className="relative z-60" ref={branchMenuRef}>
+            <div className="relative" ref={branchMenuRef}>
               <button onClick={toggleBranchesMenu} className="flex items-center space-x-1">
                 <span className="text-sm md:text-base">{selectedBranch || 'Seleccionar Sucursal'}</span>
                 <FaChevronDown className="text-xl md:text-2xl hover:text-yellow-300 transition-colors" />
               </button>
               {showBranches && (
-                <div className="absolute left-0 mt-2 w-48 bg-white text-gray-800 shadow-lg rounded-lg z-60">
+                <div className="absolute left-0 mt-2 w-48 bg-white text-gray-800 shadow-lg rounded-lg z-10">
                   {branches.length > 0 ? (
                     <ul className="max-h-48 overflow-y-auto custom-scrollbar">
                       {branches.map((branch) => (
@@ -141,14 +138,14 @@ const Header = () => {
             {/* Menú de usuario */}
             <button
               onClick={toggleUserMenu}
-              className="user-menu-button flex items-center space-x-2 bg-white text-red-600 py-1 px-3 rounded-full shadow-md hover:bg-gray-100 transition-colors z-60"
+              className="user-menu-button flex items-center space-x-2 bg-white text-red-600 py-1 px-3 rounded-full shadow-md hover:bg-gray-100 transition-colors"
             >
               <span className="text-lg font-medium">{user.name}</span>
               <FaUser className="text-2xl" />
             </button>
-            <div className="absolute top-14 right-1 z-60" ref={userMenuRef}>
+            <div className="fixed top-14 right-1" ref={userMenuRef}>
               {showUserMenu && (
-                <div className="absolute right-0 top-full mt-2 w-48 bg-white text-red-600 shadow-lg rounded-lg z-60">
+                <div className="absolute right-0 top-full mt-2 w-48 bg-white text-red-600 shadow-lg rounded-lg z-10">
                   <ul>
                     <li
                       className="px-4 py-2 hover:bg-red-100 cursor-pointer rounded-t-lg"
@@ -175,7 +172,7 @@ const Header = () => {
 
       {/* Modal de confirmación de cerrar sesión */}
       {showModal && (
-        <div className="fixed inset-0 bg-gray-500 bg-opacity-50 flex justify-center items-center z-60">
+        <div className="fixed inset-0 bg-gray-500 bg-opacity-50 flex justify-center items-center z-50">
           <div className="bg-white p-6 rounded-lg shadow-lg max-w-sm w-full">
             <h3 className="text-lg font-semibold mb-4">¿Estás seguro de que quieres cerrar sesión?</h3>
             <div className="flex justify-end space-x-4">
